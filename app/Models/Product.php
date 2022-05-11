0f1414eaeb52cd3c321b0e2db69f4022f54f6666
Ibatov Bakhtiyor <flastdtb@yandex.ru>
2022-05-11 14:17:35 +0500

    init

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $casts=[
+        'data'=>'array'
+    ];
+    protected $fillable=['name','article','status','data'];
+
+    public function scopeStatus($query)
+    {
+        return $query->where('status','available');
+    }
+}
---
+++ config/product.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'role'=>'Admin',
+        'status'=>[
+            array('id'=>0,'unavailable'),
+            array('id'=>1,'available'),
+        ],
+    ];
