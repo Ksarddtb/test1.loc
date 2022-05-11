00b4d765f170becba3e21620d1a9baee9b687534
Ibatov Bakhtiyor <flastdtb@yandex.ru>
2022-05-11 14:27:14 +0500

    header&sidebar

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
-</div>
+    <div class="nk-sidebar nk-sidebar-fixed is-dark " data-content="sidebarMenu">
+        <div class="nk-sidebar-element nk-sidebar-head">
+            <div class="nk-sidebar-brand">
+                <a href="html/index.html" class="logo-link nk-sidebar-logo">
+                    <img class="logo-light logo-img" src="{{ asset('images/logo.png') }}" srcset="./images/logo2x.png 2x"
+                        alt="logo">
+                    <img class="logo-dark logo-img" src="{{ asset('images/logo-dark.png') }}"
+                        srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                </a>
+            </div>
+            <div class="nk-menu-trigger mr-n2">
+                <a href="#" class="nk-nav-toggle nk-quick-nav-icon d-xl-none" data-target="sidebarMenu"><em
+                        class="icon ni ni-arrow-left"></em></a>
+            </div>
+        </div><!-- .nk-sidebar-element -->
+        <div class="nk-sidebar-element">
+            <div class="nk-sidebar-content">
+                <div class="nk-sidebar-menu" data-simplebar>
+                    <ul class="nk-menu">
+                        <li class="nk-menu-item active">
+                            <a href="{{route('product')}}" class="nk-menu-link">
+                                <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                                <span class="nk-menu-text">Products</span>
+                            </a>
+                        </li><!-- .nk-menu-item -->
+                    </ul><!-- .nk-menu -->
+                </div><!-- .nk-sidebar-menu -->
+            </div><!-- .nk-sidebar-content -->
+        </div><!-- .nk-sidebar-element -->
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('product','products');
+Route::view('product','products')->name('product');
 
 
 Route::middleware([
