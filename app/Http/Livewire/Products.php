b170cd422a7ad9bbf41deb552297a43821b44c60
Ibatov Bakhtiyor <ksarddtb@yandex.ru>
2022-05-12 01:32:50 +0500

    13123

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -6,7 +6,15 @@ use Livewire\Component;
 
 class Products extends Component
 {
-    public $ModalUp=false;
+    public $ModalUp=false,$product,$ProductEdit=false;
+    protected $rules=[
+        'product.article'=>'required|string|unique:products:article',
+        'product.name'=>'required|string|max:255',
+        'product.status'=>'required|string|max:12',
+        'product.data.color'=>'required|string',
+        'product.data.make'=>'required|string',
+        'product.data.size'=>'required|numeric',
+    ];
 
     public function render()
     {
@@ -14,13 +22,32 @@ class Products extends Component
             'products'=>\App\Models\Product::get(),
         ]);
     }
-    public function showModal()
+    public function showModal($id=null)
     {
-        dd('asfasfdsfsdfsdfds');
+        if($id){
+            $this->product=\App\Models\Product::whereid($id)->first();
+        }else{
+            $this->product=new \App\Models\Product();
+            $this->ProductEdit=true;
+        }
+
+        // dd($this->product);
         $this->ModalUp=true;
     }
     public function closeModal()
     {
         $this->ModalUp=false;
+        $this->ProductEdit=false;
+    }
+    public function EditProduct()
+    {
+        $this->ProductEdit=true;
+    }
+    public function store()
+    {
+        // dd($this->product);
+        $this->product->save();
+        $this->ModalUp=false;
+        $this->ProductEdit=false;
     }
 }
--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button type="button" class="btn btn-primary">Add Product</button>
+    <button type="button" class="btn btn-primary" wire:click='showModal''>Add Product</button>
     {{-- @dd($products) --}}
     <table class="table">
         <thead class="thead-dark">
@@ -9,42 +9,98 @@
                 <th scope="col">Name</th>
                 <th scope="col">Status</th>
                 <th scope="col">Date</th>
-                <th scope="col">Action</th>
+                {{-- <th scope="col">Action</th> --}}
             </tr>
         </thead>
         <tbody>
             @foreach ($products as $key => $item)
-                <tr wire:click='showModal'>
-                    <td scope="row">{{ $key++ }}</td>
-                    <td>{{ $item->article }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->status == 'available' ? 'Доступен' : 'Недоступен' }}</td>
-                    {{-- @dd($item) --}}
-                    <td>Color:{{ $item->data['color'] }} <br>
-                        Size:{{ $item->data['size'] }} <br>
-                        MadeIn:{{ $item->data['make'] }}
-                    </td>
-                    <td><button type="button" class="btn-success" wire:click='showModal'><i class="icon ni ni-pen2"></i></button></td>
-                </tr>
+            <tr wire:click='showModal({{ $item->id }})'>
+                <td scope="row">{{ $key++ }}</td>
+                <td>{{ $item->article }}</td>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->status == 'available' ? 'Доступен' : 'Недоступен' }}</td>
+                {{-- @dd($item) --}}
+                <td>Color:{{ $item->data['color'] }} <br>
+                    Size:{{ $item->data['size'] }} <br>
+                    MadeIn:{{ $item->data['make'] }}
+                </td>
+                {{-- <td><button type="button" class="btn-success" wire:click='showModal({{ $item->id }})'><i
+                            class="icon ni ni-pen2"></i></button></td> --}}
+            </tr>
             @endforeach
         </tbody>
     </table>
     @if ($ModalUp)
-        <div class="text-left modal fade show" id="default" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
-            style="padding-right: 15px; display: block;">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="myModalLabel1">Добавление/Изменение/Удаление </h4>
-                        <button type="button" class="close" wire:click='closeModal'>
-                            <span aria-hidden="true">Г—</span>
-                        </button>
+    <div class="text-left modal fade show" id="default" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+        style="padding-right: 15px; display: block;">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel1">Добавление/Изменение/Удаление </h4>
+                    @if(config('product.role')=='Admin')
+                    <button type="button" class="btn btn-success" wire:click='EditProduct'><i class="icon ni ni-pen2"></i></button>
+                    <button type="button" class="btn btn-danger"><i class="icon ni ni-trash"></i></button>
+                    @endif
+                    <button type="button" class="close" wire:click='closeModal'>
+                        <span aria-hidden="true">x</span>
+                    </button>
+                </div>
+                <form wire:submit.prevent='store'>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label class="form-label" for="default-01">Article:*
+                                @error('product.article')
+                                <span class="badge badge-danger">{{ $message }}</span>
+                                @enderror</label>
+                            <div class="form-control-wrap">
+                                <input type="text" class="form-control" id="default-01"  wire:model='product.article'  {{ $ProductEdit ? '' :'disabled' }}>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="default-01">Name:*
+                                @error('product.name')
+                                <span class="badge badge-danger">{{ $message }}</span>
+                                @enderror</label>
+                            <div class="form-control-wrap">
+                                <input type="text" class="form-control" id="default-01"  wire:model='product.name' {{ $ProductEdit ? '' :'disabled' }}>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="default-01">Status:*
+                                @error('product.status')
+                                <span class="badge badge-danger">{{ $message }}</span>
+                                @enderror</label>
+                            <div class="form-control-wrap">
+                                <select type="text" class="form-control" id="default-01"  wire:model='product.status' {{ $ProductEdit ? '' :'disabled' }}>
+                                    <option value="unavailable">Недоступен</option>
+                                    <option value="available">Доступен</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="default-01">Данные:*
+                                @error('product.data')
+                                <span class="badge badge-danger">{{ $message }}</span>
+                                @enderror</label>
+                            <div class="form-control-wrap col-md-4">
+                                <input type="text" class="form-control" id="default-01"  wire:model='product.data.color' {{ $ProductEdit ? '' :'disabled' }}>
+                            </div>
+                            <div class="form-control-wrap col-md-4">
+                                <input type="text" class="form-control" id="default-02"  wire:model='product.data.size' {{ $ProductEdit ? '' :'disabled' }}>
+                            </div>
+                            <div class="form-control-wrap col-md-4">
+                                <input type="text" class="form-control" id="default-03"  wire:model='product.data.make' {{ $ProductEdit ? '' :'disabled' }}>
+                            </div>
+                        </div>
                     </div>
-                    <form wire:submit.prevent='store'>
-                        Modal
-                    </form>
+                </form>
+                <div class="modal-action">
+                    <button type="button" class="btn grey btn-success" wire:click='store' {{ $ProductEdit ? '' :'disabled' }}>Сохранит</button>
+                    <button type="button" class="btn grey btn-danger" wire:click='closeModal'>Закрыт</button>
+                    {{-- <button type="submit" class="btn btn-outline-primary">{{ __('config.add') }}</button> --}}
                 </div>
             </div>
         </div>
+    </div>
     @endif
 </div>
